45cf6c29c00ed15c65a2ff33009b098e2e7a6f9e
aadityabhatia@gmail.com <aadityabhatia@gmail.com@9eb503c0-83de-11de-8827-c99e7a1a17c8>
2009-08-09 03:26:16 +0000

    It works.

--- robot/src/com/appspot/bartimebot/BartimebotProfileServlet.java
+++ robot/src/com/appspot/bartimebot/BartimebotProfileServlet.java
@@ -1,26 +1,29 @@
-// Copyright 2009 Google Inc. All Rights Reserved.
 package com.appspot.bartimebot;
 
 import com.google.wave.api.ProfileServlet;
 
 /**
-* A servlet that is used to fetch the profile information for Cartoony.
-* 
-*/
-public class BartimebotProfileServlet extends ProfileServlet {
+ * A servlet that is used to fetch the profile information for Cartoony.
+ * 
+ */
+@SuppressWarnings("serial")
+public class BartimebotProfileServlet extends ProfileServlet
+{
+	@Override
+	public String getRobotAvatarUrl()
+	{
+		return "http://bartimebot.appspot.com/favicon.ico";
+	}
 
-@Override
-public String getRobotAvatarUrl() {
-	return "http://bartimebot.appspot.com/favicon.ico";
-}
+	@Override
+	public String getRobotName()
+	{
+		return "BarTime!";
+	}
 
-@Override
-public String getRobotName() {
-	return "Bartime";
-}
-
-@Override
-public String getRobotProfilePageUrl() {
-	return "http://bartimebot.appspot.com";
-}
+	@Override
+	public String getRobotProfilePageUrl()
+	{
+		return "http://bartimebot.appspot.com";
+	}
 }
--- robot/src/com/appspot/bartimebot/BartimebotServlet.java
+++ robot/src/com/appspot/bartimebot/BartimebotServlet.java
@@ -1,58 +1,54 @@
 package com.appspot.bartimebot;
 
-import com.google.wave.api.AbstractRobotServlet;
-import com.google.wave.api.Blip;
-import com.google.wave.api.Event;
-import com.google.wave.api.Image;
-import com.google.wave.api.RobotMessageBundle;
-import com.google.wave.api.TextView;
-import com.google.wave.api.Wavelet;
+import java.util.logging.Logger;
+import com.appspot.bartimebot.temperature.Thermometer;
+import com.google.wave.api.*;
 
 @SuppressWarnings("serial")
-public class BartimebotServlet extends AbstractRobotServlet {
+public class BartimebotServlet extends AbstractRobotServlet
+{
+	private static final int TEMP_INITIAL = 0;
+	private static final int TEMP_THRESHOLD_WARN = 8;
+	private static final int TEMP_THRESHOLD_ACTION = 10;
 
+	private static final String WARN_TEXT = "WARNING: The heat is on. It's almost the time to grab a beer!!";
+	private static final String ACTION_GADGET_URL = "http://betagunit.com/bartime_gadget.xml";
+	
+	final static Logger LOG = Logger.getLogger(BartimebotServlet.class.getName());
 
 	@Override
-	public void processEvents(RobotMessageBundle bundle) {
-		for (Event blipSubmittedEvent : bundle.getBlipSubmittedEvents()) {
-	    	makeCartoonBlip(blipSubmittedEvent.getBlip());
-	    }
-		for(Event e : bundle.getEvents()) {
-			e.getWavelet().appendBlip().getDocument().append( "there was an " + String.valueOf(e.getType()) + " event" );
+	public void processEvents(RobotMessageBundle bundle)
+	{
+		Wavelet wavelet = bundle.getWavelet();
+
+		if (bundle.wasSelfAdded())
+		{
+			LOG.warning("I was added!!!");
+			wavelet.setDataDocument("temperature", TEMP_INITIAL + "");
+		}
+
+		for (Event event : bundle.getBlipSubmittedEvents())
+		{
+			int temperature = Integer.parseInt(wavelet.getDataDocument("temperature"));
+			Blip blip = event.getBlip();
+			temperature += Thermometer.getTemperatureDifference(blip.getDocument().getText());
+			wavelet.setDataDocument("temperature", "" + temperature);
+			LOG.warning("Conversation temperature: " + temperature);
+			
+			if(temperature >= TEMP_THRESHOLD_WARN && !wavelet.hasDataDocument("warned"))
+			{
+				Blip warnBlip = wavelet.appendBlip();
+				warnBlip.getDocument().append(WARN_TEXT);
+				wavelet.setDataDocument("warned", "1");
+			}
+			
+			if(temperature >= TEMP_THRESHOLD_ACTION && !wavelet.hasDataDocument("beertime"))
+			{
+				Blip warnBlip = wavelet.appendBlip();
+				Gadget actionGadget = new Gadget(ACTION_GADGET_URL);
+				warnBlip.getDocument().getGadgetView().append(actionGadget);
+				wavelet.setDataDocument("beertime", "1");
+			}
 		}
 	}
-	
-	public void makeDebugBlip(Wavelet wavelet, String text) {
-		TextView textView = wavelet.appendBlip().getDocument();
-		textView.append(text + " in a cartoon!");
-	}
-	
-	public void makeCartoonBlip(Blip blip) {
-		String creator = blip.getCreator();
-		String text = blip.getDocument().getText();
-		Image image = new Image();
-		image.setUrl(makeCartoonUrl(text, creator));
-		
-		blip.getDocument().delete();
-		blip.getDocument().appendElement(image);
-	}
-	
-	public String makeCartoonUrl(String text, String creator) {
-		String base = "http://www.google.com/chart?chst=d_bubble_texts_big&chld=bb|" + makeColor(creator) + "|FFFFFF";
-		base += "|" + text.replace(" ", "+");
-		
-		return base;
-	}
-	
-	public String makeColor(String name) {
-		String hex[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
-		String color = "";
-		for (int i = 0; i < Math.min(name.length(), 6); i++) {
-			char code = name.charAt(i);
-			int scaled = code % 16;
-			color += hex[scaled];
-		}
-		return color;
-	}
-	
 }
